4d3be8e917984f852172c40e4b73d5b3ebd8633b
Your Name <you@example.com>
2023-11-15 17:21:38 +0700

    fix node

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,58 +1,92 @@
-const {Author,Game} = require("../../src/model/model")
+const { Author, Game } = require("../../src/model/model");
 
 const gameController = {
-    addGame : async(req,res)=>{
+    addGame: async (req, res) => {
         try {
-            const game = new Game(req.body)
-            const saveGame = await game.save()
-            if(req.body.publisher){
-                // const author = Author.find({_id:res.body.author})
-                const author = Author.findById(req.body.publisher)
-              await  author.updateOne({$push : {games:saveGame._id}})
+            const game = new Game(req.body);
+            const savedGame = await game.save();
+
+            if (req.body.publisher) {
+                const author = await Author.findById(req.body.publisher);
+
+                if (!author) {
+                    return res.status(404).json({ error: "Author not found" });
+                }
+
+                await author.updateOne({ $push: { games: savedGame._id } });
             }
-            res.status(200).json(saveGame)
+
+            res.status(201).json(savedGame);
         } catch (err) {
-            res.status(500).json(err)
+            console.error(err);
+            res.status(500).json({ error: "Internal Server Error" });
         }
     },
 
-    getGame : async(req,res)=>{
+    getGame: async (req, res) => {
         try {
-                const games =  await Game.find().populate("publisher");
-                res.status(200).json(games)
-            } catch (err) {
-            res.status(500).json(err)
-        }
-    },
-    getAnGame : async (req,res)=>{
-        try {
-                const game = await Game.findById(req.params.id).populate("publisher")
-                res.status(200).json(game)
+            const games = await Game.find().populate("publisher");
+            res.status(200).json(games);
         } catch (err) {
-            res.status(500).json(err)
+            console.error(err);
+            res.status(500).json({ error: "Internal Server Error" });
         }
     },
-    updateAnGame : async (req,res)=>{
+
+    getAnGame: async (req, res) => {
         try {
-                const game = await Game.findById(req.params.id)
-                await game.updateOne({$set : req.body})
-                res.status(200).json("update susscessfully!!!")
+            const game = await Game.findById(req.params.id).populate("publisher");
+
+            if (!game) {
+                return res.status(404).json({ error: "Game not found" });
+            }
+
+            res.status(200).json(game);
         } catch (err) {
-            res.status(500).json(err)
+            console.error(err);
+            res.status(500).json({ error: "Internal Server Error" });
         }
     },
-    destroyAnGame: async (req,res)=>{
-        try { 
+
+    updateAnGame: async (req, res) => {
+        try {
+            const game = await Game.findByIdAndUpdate(req.params.id, req.body, {
+                new: true, // Return the updated document
+                runValidators: true, // Run validators on update
+            });
+
+            if (!game) {
+                return res.status(404).json({ error: "Game not found" });
+            }
+
+            res.status(200).json({ message: "Update successful", updatedGame: game });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    },
+
+    destroyAnGame: async (req, res) => {
+        try {
+            // Remove the game from authors' books array
             await Author.updateMany(
-                { books : req.params.id },
-                { $pull: req.params.id }
-                )
-            await Game.findByIdAndDelete(req.params.id)
-            res.status(200).json("delete susscessfully!!!")
+                { games: req.params.id },
+                { $pull: { games: req.params.id } }
+            );
+
+            // Delete the game
+            const deletedGame = await Game.findByIdAndDelete(req.params.id);
+
+            if (!deletedGame) {
+                return res.status(404).json({ error: "Game not found" });
+            }
+
+            res.status(200).json({ message: "Delete successful" });
         } catch (err) {
-            res.status(500).json(err)
-            
+            console.error(err);
+            res.status(500).json({ error: "Internal Server Error" });
         }
-    }
-}
-module.exports = gameController
+    },
+};
+
+module.exports = gameController;
